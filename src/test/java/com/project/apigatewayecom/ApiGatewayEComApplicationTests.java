cc1368247aa5a19db88c257f4d5d3dadb16dcc8d
Dhananjay Bhadke <bhadke443@gmail.com>
2024-06-21 19:51:28 +0530

    API Gateway

---
+++ src/test/java/com/project/apigatewayecom/ApiGatewayEComApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.apigatewayecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayEComApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
